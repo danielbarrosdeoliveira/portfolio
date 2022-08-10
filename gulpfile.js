936b244446ba4a5bd406575980bdaec347f88fd3
Daniel Oliveira <d.dani_oli@hotmaiil.com>
2022-08-10 03:12:15 -0300

    :wrench: adjustment gulp to generate a dist folder to build

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,20 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require("browser-sync").create();
-const concat = require("gulp-concat");
-const babel = require("gulp-babel");
-const uglify = require("gulp-uglify");
+
+function copyHtml() {
+  return gulp
+    .src("./src/index.html")
+    .pipe(gulp.dest("./dist/"))
+    .pipe(browserSync.stream());
+}
+
+function copyAssets() {
+  return gulp
+    .src("./src/assets/**/*.*")
+    .pipe(gulp.dest("./dist/assets/"))
+    .on("end", browserSync.reload);
+}
 
 function compileSass() {
   return gulp
@@ -15,25 +26,34 @@ function compileSass() {
         cascade: true,
       })
     )
-    .pipe(gulp.dest("./src/css/"))
+    .pipe(gulp.dest("./dist/css/"))
     .pipe(browserSync.stream());
 }
 
 function browser() {
   browserSync.init({
     server: {
-      baseDir: "./src",
+      baseDir: "./dist/",
     },
   });
 }
 
 function watch() {
-  gulp.watch("./src/*.html").on("change", browserSync.reload);
+  gulp.watch("./src/*.html").on("change", copyHtml);
   gulp.watch("./src/scss/**/*.scss", compileSass);
+  gulp.watch("./src/assets/**/*.*", copyAssets);
 }
 
 exports.compileSass = compileSass;
 exports.browser = browser;
 exports.watch = watch;
 
-exports.default = gulp.parallel(browser, compileSass, watch);
+exports.default = gulp.parallel(
+  browser,
+  copyHtml,
+  copyAssets,
+  compileSass,
+  watch
+);
+
+exports.build = gulp.parallel(copyHtml, copyAssets, compileSass);
